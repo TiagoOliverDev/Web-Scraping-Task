b49c95354f4bf64c2be447b36ea431a492823472
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-02-14 08:43:21 -0300

    feat: habilitando main

---
+++ main.py
@@ -0,0 +1,13 @@
+from src.utils import Web_scraping_aux
+
+class Parse_veri_bet:
+
+    robot_scraping = Web_scraping_aux()
+
+    def start_robot(self):
+        xpath_base = '//*[@id="odds-picks"]/tbody'
+        self.robot_scraping.data_extract_bet(xpath_base)
+
+if __name__ == "__main__":
+    parser = Parse_veri_bet()
+    parser.start_robot()
